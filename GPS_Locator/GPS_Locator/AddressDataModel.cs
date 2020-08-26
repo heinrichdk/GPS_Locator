fa663174368784dccd84a7e24e5c3275b82af68c
Heinrich De Klerk <heinrichdk3030@gmail.com>
2020-08-26 14:28:28 +0200

    Add project files.

---
+++ GPS_Locator/GPS_Locator/AddressDataModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GPS_Locator
+{
+    public class AddressDataModel
+    {
+        public string continent { get => continent; set => continent = value; }
+        public string countryName { get => countryName; set => countryName = value; }
+        public string city { get => city; set => city = value; }
+        public string locality { get => locality; set => locality = value; }
+        public string postcode { get => postcode; set => postcode = value; }
+    }
+}
---
+++ GPS_Locator/GPS_Locator/GPSClass.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+namespace GPS_Locator
+{
+    public  class GPSClass
+    {
+        static HttpClient client = new HttpClient();
+        private async Task<string> getAddress()
+        {
+            try
+            {
+                var request = new GeolocationRequest(GeolocationAccuracy.Medium);
+                var location = await Geolocation.GetLocationAsync(request);
+                string address = null;
+                if (location != null)
+                {
+                    HttpResponseMessage response = await client.GetAsync(String.Format("https://api.bigdatacloud.net/data/reverse-geocode-client?latitude={0}&longitude={1}&localityLanguage=en", location.Latitude, location.Longitude));
+                    if (response.IsSuccessStatusCode)
+                    {
+                        address = await response.Content.ReadAsStringAsync();
+                    }
+                }
+                return address;
+            }
+            catch(Exception ex)
+            {
+                return null;
+            }
+        }
+        public  AddressDataModel DeserializeAdressObject()
+        {
+
+            AddressDataModel address = new AddressDataModel();
+            string json = getAddress().ToString();
+            if(json != null)
+            {
+                address = JsonSerializer.Deserialize<AddressDataModel>(json);
+            }
+            return address;
+        }
+    }
+  
+}
